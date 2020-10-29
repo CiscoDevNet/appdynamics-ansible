b3a18dabb93c3ea263011c9add7fedd9e1d02d0f
Israel Ogbole <iogbole@yahoo.com>
2020-10-29 12:03:42 +0000

    sematic

--- install_collection.sh
+++ install_collection.sh
@@ -3,8 +3,12 @@
 rm -rf ~/.ansible/collections/ansible_collections/appdynamics #uninstall the collection if it exists
 
 #build a new one 
+echo "Build collection"
 collection_file=$(basename $(ansible-galaxy collection build -f | awk -F" " '{print $NF}'))
 
+echo "Built file - ${collection_file}"
+
+echo "Install collection from - ${collection_file}"
 ansible-galaxy collection install "${collection_file}"
 
 rm -rf "${collection_file}"
