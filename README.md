95dfec44e8300e914be37ba5ec3f90023e69afcb
Israel Ogbole <iogbole@yahoo.com>
2020-10-29 12:07:22 +0000

    molecule tests (#1)
    sanity test, molecule test

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# AppDynamics Ansible Playbooks
+# Ansible Collection - appdynamics.appdynamics
 
-### [Documentation is in progress]
-
-These examples uses AWS S3 and Nexus as binary stores. You easily adapt/change it to suite your needs.. 
-
-# Linux
-ansible-playbook linux-role.yaml 
-
-# Windows
-ansible-playbook win-role.yaml 
-
-# DB Agent 
-ansible-playbook db-role.yaml 
+Documentation for the collection.
---
+++ install_collection.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.ansible/collections/ansible_collections/appdynamics #uninstall the collection if it exists
+
+#build a new one 
+echo "Build collection"
+collection_file=$(basename $(ansible-galaxy collection build -f | awk -F" " '{print $NF}'))
+
+echo "Built file - ${collection_file}"
+
+echo "Install collection from - ${collection_file}"
+ansible-galaxy collection install "${collection_file}"
+
+rm -rf "${collection_file}"
+
+ 
