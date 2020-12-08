76a24ab0bce556aefbae57fff59a7364a8aaa722
Israel Ogbole <iogbole@yahoo.com>
2020-12-08 19:29:49 +0000

    shebang fix

--- roles/jq/files/get-checksum.sh
+++ roles/jq/files/get-checksum.sh
@@ -1,19 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
 VER=1.6
 DIR=~/Downloads
 MIRROR=https://github.com/stedolan/jq/releases/download/jq-$VER
 
-dl()
+ch()
 {
     PLATFORM=$1
     SUFFIX=${2:-}
     wget -O $DIR/jq-$VER-$PLATFORM$SUFFIX $MIRROR/jq-$PLATFORM
 }
 
-dl linux32
-dl linux64
-dl osx-amd64
-dl win32 .exe
-dl win64 .exe
+ch linux32
+ch linux64
+ch osx-amd64
+ch win32 .exe
+ch win64 .exe
 sha256sum $DIR/jq-$VER-*
 
