18e5be74c4a04c445bde9ca692ddf0d6750c3cba
Israel Ogbole <iogbole@yahoo.com>
2020-12-08 20:19:34 +0000

    Install jq role  (#41)
    * add jq role
    * add jq checksum

---
+++ roles/jq/files/get-checksum.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+VER=1.6
+DIR=~/Downloads
+MIRROR=https://github.com/stedolan/jq/releases/download/jq-"$VER"
+
+ch()
+{
+    PLATFORM="$1"
+    SUFFIX="${2:-}"
+    wget -O "$DIR"/jq-"$VER"-"$PLATFORM""$SUFFIX" "$MIRROR"/jq-"$PLATFORM"
+}
+
+ch linux32
+ch linux64
+ch osx-amd64
+ch win32 .exe
+ch win64 .exe
+sha256sum "$DIR"/jq-"$VER"-*
+
